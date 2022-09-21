913a78817b38567f008deafa65e81eaa4011667b
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-09-21 15:46:14 +0300

    add components

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -4,10 +4,9 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Header } from './Header';
 
 export default {
-  title: 'Example/Header',
+  title: 'Components/Header',
   component: Header,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
 } as ComponentMeta<typeof Header>;
